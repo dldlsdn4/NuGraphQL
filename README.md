1965dd16c5996a2890545151ecef06b5074a5b4e
dldlsdn4 <dldlsdn4@naver.com>
2022-10-21 20:50:04 +0900

    20221021 GraphQL Practice

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # NuGraphQL
 graphql + flutter
 
+# query website
+https://countries.trevorblades.com/
+
+# 참고사이트
 https://github.com/zino-hofmann/graphql-flutter
--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,73 @@
 import 'package:flutter/material.dart';
-import './graphql_bloc/main.dart' show GraphQLBlocPatternScreen;
-import './graphql_widget/main.dart' show GraphQLWidgetScreen;
-import 'fetchmore/main.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() => runApp(
-      MaterialApp(
-        title: 'GraphQL Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        debugShowCheckedModeBanner: false,
-        home: Builder(
-          builder: (BuildContext context) => Scaffold(
-            appBar: AppBar(
-              title: const Text('GraphQL Demo App'),
-            ),
-            body: Center(
-              child: Column(
-                children: <Widget>[
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute<GraphQLWidgetScreen>(
-                          builder: (BuildContext context) =>
-                              GraphQLBlocPatternScreen(),
-                        ),
-                      );
-                    },
-                    child: const Text('GraphQL BloC pattern'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute<GraphQLWidgetScreen>(
-                          builder: (BuildContext context) =>
-                              const GraphQLWidgetScreen(),
-                        ),
-                      );
-                    },
-                    child: const Text('GraphQL Widget'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute<FetchMoreWidgetScreen>(
-                          builder: (BuildContext context) =>
-                              const FetchMoreWidgetScreen(),
-                        ),
-                      );
-                    },
-                    child: const Text('Fetchmore (Pagination) Example'),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "GQL App",
+      home: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final HttpLink httpLink = HttpLink("https://countries.trevorblades.com/");
+    final ValueNotifier<GraphQLClient> client = ValueNotifier<GraphQLClient>(
+        GraphQLClient(link: httpLink, cache: GraphQLCache()));
+
+    return GraphQLProvider(
+      child: HomePage(),
+      client: client,
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("GraphQL Client")),
+      body: Query(
+        options: QueryOptions(document: gql(r'''
+          query{
+              country(code:"KR"){
+              name
+              code
+              phone
+              currency
+              continent{
+                name
+                code
+              }
+              languages{
+                name
+              }
+            }
+        }''')),
+        builder: (result, {fetchMore, refetch}) {
+          if (result.data == null) {
+            return Text("No Data Found !");
+          }
+          return ListView.builder(
+            itemBuilder: (context, index) {
+              return Text(result.data.toString());
+            },
+            itemCount: result.data?.length,
+          );
+        },
       ),
     );
+  }
+}
